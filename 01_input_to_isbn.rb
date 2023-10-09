673c316edbdc0e4c7ebde76a7f33107c86d6bc44
Nakashoya Hidesys Megumi <nakashoya@hidetaro.com>
2023-10-09 21:18:24 +0900

    Rubocop

--- 01_input_to_isbn.rb
+++ 01_input_to_isbn.rb
@@ -1,9 +1,9 @@
-require "yaml"
+require 'yaml'
 
-INPUT_GLOB = "../01_input/**/*.pdf"
+INPUT_GLOB = '../01_input/**/*.pdf'.freeze
 
 class PdfProcessor
-  OUTPUT_DIR = "../02_isbn"
+  OUTPUT_DIR = '../02_isbn'.freeze
   def initialize(input_path)
     @input_path = input_path
   end
@@ -15,10 +15,11 @@ class PdfProcessor
     zbars = read_barcode(cover_temp_name)
     File.delete(cover_temp_name)
     metadata = generate_meta(zbars)
-    output_path = generate_output_path(metadata["isbn13"], metadata["magazine_code"], metadata["author"], metadata["translater"], metadata["title"], metadata["ocr"], metadata["original_file_name"])
+    output_path = generate_output_path(metadata['isbn13'], metadata['magazine_code'], metadata['author'],
+                                       metadata['translater'], metadata['title'], metadata['ocr'], metadata['original_file_name'])
 
     puts "  move to: #{output_path}"
-    YAML.dump(metadata, File.open(output_path + ".yml", "w"))
+    YAML.dump(metadata, File.open("#{output_path}.yml", 'w'))
     File.rename(@input_path, output_path)
   end
 
@@ -28,73 +29,73 @@ class PdfProcessor
     last_page = `pdfimages -list \"#{@input_path}\"`.split("\n").last
     return false if last_page !~ /^\s*(\d+)\s/
 
-    `pdfimages -f #{$1} -j \"#{@input_path}\" ./tmp/`
-    cover_path = Dir.glob("./tmp/*.jpg").first
-    cover_path ||= Dir.glob("./tmp/*.ppm").first
-    cover_path ||= Dir.glob("./tmp/*.pbm").first
-    cover_ext = cover_path.split(".").last
+    `pdfimages -f #{::Regexp.last_match(1)} -j \"#{@input_path}\" ./tmp/`
+    cover_path = Dir.glob('./tmp/*.jpg').first
+    cover_path ||= Dir.glob('./tmp/*.ppm').first
+    cover_path ||= Dir.glob('./tmp/*.pbm').first
+    cover_ext = cover_path.split('.').last
     cover_temp_name = "./tmp/input.#{cover_ext}"
     File.rename(cover_path, cover_temp_name)
     cover_temp_name
   end
 
   def read_barcode(cover_temp_name)
-    `zbarimg -q #{cover_temp_name}`.split("\n").map{|zbar| zbar =~ /\:(\d+)$/ ? $1 : nil}.compact
+    `zbarimg -q #{cover_temp_name}`.split("\n").map { |zbar| zbar =~ /:(\d+)$/ ? ::Regexp.last_match(1) : nil }.compact
   end
 
   def generate_meta(zbars)
-    #メタデータ処理
-    isbn13 = zbars.find{|zbar| zbar =~ /^(97[89]\d+)$/}
-    magazine_code = zbars.find{|zbar| zbar =~ /^(491\d+)$/}
-    original_file_path = @input_path =~ /^\.\.\/01_input\/(.+)$/ && $1
-    tag = original_file_path =~ /^(.+?)\// && $1
-    original_file_name = original_file_path.split("/").last
-    author = original_file_name =~ /^\[(.+?)(（.+）)?\]/ && $1
-    translater = $2 && $2[1..-2]
-    title = original_file_name =~ /\](.+?)(（OCR済）)?\.pdf$/i && $1
-    ocr = $2
-    volume = title ? (title =~ /^.+?(\d+)$/ ? $1.to_i : nil) : nil
+    # メタデータ処理
+    isbn13 = zbars.find { |zbar| zbar =~ /^(97[89]\d+)$/ }
+    magazine_code = zbars.find { |zbar| zbar =~ /^(491\d+)$/ }
+    original_file_path = @input_path =~ %r{^\.\./01_input/(.+)$} && ::Regexp.last_match(1)
+    tag = original_file_path =~ %r{^(.+?)/} && ::Regexp.last_match(1)
+    original_file_name = original_file_path.split('/').last
+    author = original_file_name =~ /^\[(.+?)(（.+）)?\]/ && ::Regexp.last_match(1)
+    translater = ::Regexp.last_match(2) && ::Regexp.last_match(2)[1..-2]
+    title = original_file_name =~ /\](.+?)(（OCR済）)?\.pdf$/i && ::Regexp.last_match(1)
+    ocr = ::Regexp.last_match(2)
+    volume = if title
+               title =~ /^.+?(\d+)$/ ? ::Regexp.last_match(1).to_i : nil
+             end
     {
-      "zbars" => zbars,
-      "isbn13" => isbn13,
-      "magazine_code" => magazine_code,
-      "original_file_path" => original_file_path,
-      "tags" => (tag ? [tag] : []),
-      "original_file_name" => original_file_name,
-      "author" => author,
-      "translater" => translater,
-      "title" => title,
-      "ocr" => !!ocr,
-      "volume" => volume
+      'zbars' => zbars,
+      'isbn13' => isbn13,
+      'magazine_code' => magazine_code,
+      'original_file_path' => original_file_path,
+      'tags' => (tag ? [tag] : []),
+      'original_file_name' => original_file_name,
+      'author' => author,
+      'translater' => translater,
+      'title' => title,
+      'ocr' => !ocr.nil?,
+      'volume' => volume
     }
   end
 
   def generate_output_path(isbn13, magazine_code, author, translater, title, ocr, original_file_name)
     output_path = nil
     if isbn13
-      output_path = "#{OUTPUT_DIR}/isbn13/#{ocr ? 'ocred' : "un_ocr/"}#{isbn13}.pdf"
+      output_path = "#{OUTPUT_DIR}/isbn13/#{ocr ? 'ocred' : 'un_ocr/'}#{isbn13}.pdf"
     elsif magazine_code
-      output_path = "#{OUTPUT_DIR}/magazine/#{ocr ? 'ocred' : "un_ocr/"}#{magazine_code}.pdf"
+      output_path = "#{OUTPUT_DIR}/magazine/#{ocr ? 'ocred' : 'un_ocr/'}#{magazine_code}.pdf"
     elsif author && title
-      output_path = "#{OUTPUT_DIR}/title_by_hand/#{ocr ? 'ocred' : "un_ocr/"}[#{author}#{translater ? "（#{translater}）" : nil}]#{title}.pdf"
+      output_path = "#{OUTPUT_DIR}/title_by_hand/#{ocr ? 'ocred' : 'un_ocr/'}[#{author}#{translater ? "（#{translater}）" : nil}]#{title}.pdf"
     else
-      output_path = "#{OUTPUT_DIR}/no_isbn_no_title/#{ocr ? 'ocred' : "un_ocr/"}#{original_file_name}"
+      output_path = "#{OUTPUT_DIR}/no_isbn_no_title/#{ocr ? 'ocred' : 'un_ocr/'}#{original_file_name}"
     end
-    while File.exists?(output_path)
-      if output_path !~ /_(\d+)\.pdf$/
-        output_path.gsub!(/\.pdf$/, "_1.pdf")
-      end
-      i = $1.to_i
-      output_path.gsub!(/\_#{i}\.pdf$/, "_#{i + 1}.pdf")
+    while File.exist?(output_path)
+      output_path.gsub!(/\.pdf$/, '_1.pdf') if output_path !~ /_(\d+)\.pdf$/
+      i = ::Regexp.last_match(1).to_i
+      output_path.gsub!(/_#{i}\.pdf$/, "_#{i + 1}.pdf")
     end
+    output_path
   end
 end
 
-
-puts "Start."
-file_list = Dir.glob(INPUT_GLOB).sort
-file_list.each.with_index do |@input_path, file_index|
+puts 'Start.'
+file_list = Dir.glob(INPUT_GLOB)
+file_list.each.with_index do |_input_path, file_index|
   puts "Process (#{file_index + 1}/#{file_list.length}): #{@input_path}"
   PdfProcessor.new(@input_path).process
 end
-puts "Finish."
+puts 'Finish.'
--- 03_make_symbolic_links.rb
+++ 03_make_symbolic_links.rb
@@ -1,82 +1,69 @@
-require "yaml"
+require 'yaml'
 
 class String
   def get_match(regex)
-    if self =~ regex
-      if $1
-        $1
-      else
-        raise "No Regex pattern cache. :" + regex.to_s
-      end
-    else
-      raise "Didn't match: " + self
-    end
+    raise "Didn't match: #{self}" unless self =~ regex
+    raise "No Regex pattern cache. :#{regex}" unless ::Regexp.last_match(1)
+
+    ::Regexp.last_match(1)
   end
 end
 
-Dir.glob("../original/**/*.pdf").shuffle.each do |pdf_path|
+Dir.glob('../original/**/*.pdf').shuffle.each do |pdf_path|
   ln_name = nil
   ln_tags = []
-  pdf_yml_path = pdf_path + ".yml"
+  pdf_yml_path = "#{pdf_path}.yml"
   pdf_yml = nil
-  if File.exists?(pdf_yml_path)
+  if File.exist?(pdf_yml_path)
     pdf_yml = YAML.load_file(pdf_yml_path)
-    ln_tags = pdf_yml["tags"]
+    ln_tags = pdf_yml['tags']
   end
-  if pdf_yml && pdf_yml["title"] && pdf_yml["authors"] && !pdf_yml["authors"].empty?
-    ln_name = "[#{pdf_yml["authors"].join(",")}#{pdf_yml["translator"] ? "（#{pdf_yml["translator"]}）" : nil}]#{pdf_yml["title"]}"
+  if pdf_yml && pdf_yml['title'] && pdf_yml['authors'] && !pdf_yml['authors'].empty?
+    ln_name = "[#{pdf_yml['authors'].join(',')}#{pdf_yml['translator'] ? "（#{pdf_yml['translator']}）" : nil}]#{pdf_yml['title']}"
   else
     case pdf_path
-    when /\/by_hand\//
+    when %r{/by_hand/}
       raise pdf_path
-    when /\/isbn13\//, /\/magazine\//
-      isbn_or_jan = pdf_path.get_match(/\/(\d+)(:?_\d)?\.pdf$/)
+    when %r{/isbn13/}, %r{/magazine/}
+      isbn_or_jan = pdf_path.get_match(%r{/(\d+)(:?_\d)?\.pdf$})
       aws_info_path = "../original/aws_meta/#{isbn_or_jan}.yml"
       item = nil
-      if File.exists?(aws_info_path)
+      if File.exist?(aws_info_path)
         aws_info = YAML.load_file(aws_info_path)
-        item = aws_info["Items"]["Item"]
+        item = aws_info['Items']['Item']
       end
       if item
-        if item.is_a?(Array)
-          item = item.find{|it| it["ItemAttributes"]["Binding"] !~ /Kindle/}
-        end
-        item_attr = item["ItemAttributes"]
-        author = item_attr["Author"] || item_attr["Studio"]
-        if author.is_a?(Array)
-          author = author.join(",")
-        end
-        author && author.gsub!(/([^a-zA-Z])\s([^a-zA-Z])/, "\\1\\2")
-        title = item_attr["Title"]
+        item = item.find { |it| it['ItemAttributes']['Binding'] !~ /Kindle/ } if item.is_a?(Array)
+        item_attr = item['ItemAttributes']
+        author = item_attr['Author'] || item_attr['Studio']
+        author = author.join(',') if author.is_a?(Array)
+        author&.gsub!(/([^a-zA-Z])\s([^a-zA-Z])/, '\\1\\2')
+        title = item_attr['Title']
         ln_name = "[#{author}]#{title}"
-        ln_tags << item_attr["Binding"].gsub(/（.+）/, "").gsub("コミック", "漫画")
+        ln_tags << item_attr['Binding'].gsub(/（.+）/, '').gsub('コミック', '漫画')
       else
         ln_name = isbn_or_jan
       end
-    when /\/no_isbn\//
-      ln_name = pdf_path.get_match(/\/([^\/]+)\.pdf$/)
+    when %r{/no_isbn/}
+      ln_name = pdf_path.get_match(%r{/([^/]+)\.pdf$})
     else
       raise pdf_path
     end
   end
-  if ln_tags.empty?
-    ln_tags << "未整理"
-  end
+  ln_tags << '未整理' if ln_tags.empty?
   ln_tags.uniq.each do |tag|
     dir_path = "../output/#{tag}"
-    if !Dir.exists?(dir_path)
-      Dir.mkdir(dir_path)
-    end
-    ln_name_clean = ln_name.gsub(/[\\\/\:\*\?\<\>\"\|]/, "_").tr("０-９Ａ-Ｚａ-ｚ（）", "0-9A-Za-z()")
+
+    FileUtils.mkdir_p(dir_path)
+
+    ln_name_clean = ln_name.gsub(%r{[\\/\:\*\?\<\>\"\|]}, '_').tr('０-９Ａ-Ｚａ-ｚ（）', '0-9A-Za-z()')
     ln_path = "#{dir_path}/#{ln_name_clean}.pdf"
-    while File.exists?(ln_path)
-      if ln_path !~ /_(\d+)\.pdf$/
-        ln_path.gsub!(/\.pdf$/, "_1.pdf")
-      end
-      i = $1.to_i
-      ln_path.gsub!(/\_#{i}\.pdf$/, "_#{i + 1}.pdf")
+    while File.exist?(ln_path)
+      ln_path.gsub!(/\.pdf$/, '_1.pdf') if ln_path !~ /_(\d+)\.pdf$/
+      i = Regexp.last_match(1).to_i
+      ln_path.gsub!(/_#{i}\.pdf$/, "_#{i + 1}.pdf")
     end
-    puts pdf_path + " : " + ln_path
+    puts "#{pdf_path} : #{ln_path}"
     p `ln -s ../#{pdf_path} \"#{ln_path}\"`
   end
 end
